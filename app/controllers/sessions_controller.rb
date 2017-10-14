3e1b0f4b6c2b2264fdb14591adc67367c62da51d
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-10-14 08:53:30 +0000

    Log user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+
+    redirect_to root_path
   end
 
   def destroy
-    session.delete :name
+    session.clear
+    redirect_to root_path
   end
 end
