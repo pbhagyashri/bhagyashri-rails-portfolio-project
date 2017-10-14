d4143c5ca54ab919614efb55deb836a41f080ec1
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-10-14 09:47:51 +0000

    Can Authenticate user's password after login and render message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,18 @@
 class SessionsController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
     @user = User.find_by(email: params[:email])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-
-    redirect_to root_path
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:message] = "You are logged in successfully"
+      redirect_to root_path
+    else
+      flash[:message] = "Invalid Email or Password."
+      render :new
+    end
   end
 
   def destroy
