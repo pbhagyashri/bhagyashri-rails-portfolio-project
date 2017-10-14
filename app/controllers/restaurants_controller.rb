a5c7e185fae8387feada8a70cc95b1f075d225d6
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-10-14 11:26:09 +0000

    Add form partial to edit and create restaurants, add partials to display details on show and index page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,42 @@
 class RestaurantsController < ApplicationController
-  def new
 
+  before_action :set_restaurant, only: [:edit, :show, :destroy, :update]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :cuisine)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
   end
 
 end
