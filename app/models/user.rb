41938d45949877c89c4d0c7f1d5c59b43df845d0
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-10-30 20:54:56 +0000

    Refactoring with partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, :presence => true
   validates :email, :uniqueness => true
   validates :username, :presence => true
-  validates :username, :uniqueness => true
+  # validates :username, :uniqueness => true
 
   has_secure_password
 
