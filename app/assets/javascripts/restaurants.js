5daa03394addaba4fe1616ad50f25666a2a50c23
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-02-17 02:25:13 +0000

    Add Restaurant constructor function and addSpices and bakeReviews prototype function to restaurant.js file. Can append restaurants and reviews to restaurant-container div

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,23 +1,77 @@
-
-$(() => {
+$(document).ready(function() {
+ 
   bindClickHandlers()
-})
-
-
-    
-
+  reloadHomePage()
+  
+});
 
 const bindClickHandlers = () => {
   
-  $('#list_restaurants').on('click', (event) => {
+  $('#list_restaurants').on('click', function(event) {
     
     event.preventDefault();
     
+    history.pushState(null, null, "restaurants")
     $.get('/restaurants.json').done((restaurants) => {
-      var restaurantDiv = $("#allRestaurants")
-    });
+      $("#restaurant-container").html('')
+      restaurants.forEach( (restaurant) => {
+        let cookRestaurant = new Restaurant(restaurant)
+        
+        let seasoneRestaurant = cookRestaurant.addSpices()
     
+        $('#restaurant-container').append(seasoneRestaurant)
+        debugger
+        $('#restaurant-container').append(cookRestaurant.bakeReviews())
+        
+      })//forEach
+    });//get
+    
+  })//onClick
+  
+}//bindClickHandler
+
+function Restaurant(restaurant) {
+  this.id = restaurant.id
+  this.name = restaurant.name
+  this.location = restaurant.location
+  this.cuisine = restaurant.cuisine
+  this.reviews = restaurant.reviews
+} //constructor
+
+Restaurant.prototype.addSpices = function() {
+  let seasoneRestaurant = `
+    <div id="rest_div">
+      <h2><b>${this.name}</b></h2>
+      <h4><b>Location:</b> ${this.location} </h4>
+      <h4><b>cuisine:</b> ${this.cuisine} </h4>
+     
+    </div>
+  `
+  return seasoneRestaurant
+} // prototype
+
+Restaurant.prototype.bakeReviews = function () {
+  let reviews = this.reviews
+  
+  let reviewsBatch = ''
+  
+  reviews.forEach((review) => {
+    let reviewloaf = `
+      <div id="reviewsDiv">
+        <h5><b>Taste Rating:</b>${review.taste_rating}</h5>
+        <h5><b>Health Rating:</b>${review.health_rating}</h5>
+        <h5><b>Cleanliness Rating:</b>${review.cleanliness_rating}</h5>
+        <h5><b>Discription:</b>${review.description}</h5>
+      </div>
+    `
+    reviewsBatch += reviewloaf
   })
   
-  
+  return reviewsBatch
+}
+
+function reloadHomePage() {
+  $("#home-button").on("click", (e) => {
+    location.reload();
+  });
 }
