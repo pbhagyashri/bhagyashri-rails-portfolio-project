3afa6357ff563f43a4b0ca424d081ecedff4c53b
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-07-15 02:22:31 -0400

    Add Images and styling to Header.js file

--- better-meals-client/src/components/Header.js
+++ better-meals-client/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import food from '../annie-spratt-44241-unsplash.jpg'
 
 const Header = () => {
   return (
     <div>
-      <h1>Beeter Meals</h1>
+      <div className="header-div">
+        <h1 className="logo5">Beter Meals</h1>
+      </div>
+      <h2>Restaurants</h2>
     </div>
   )
 }
